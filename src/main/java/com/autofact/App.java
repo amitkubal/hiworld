539302422e3ce71c16650d177ad040c564df5621
amitkubal <amit.kubal79@gmail.com>
2017-06-30 10:41:10 +0530

    simple-hiworld

---
+++ src/main/java/com/autofact/App.java
@@ -0,0 +1,13 @@
+package com.autofact;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
